4582128cf67a78533ee7074d30090423268bf544
Franck Cassedanne <franck@ouarz.net>
2015-06-01 18:55:57 +0100

    i

--- Casks/coronasdk-nightly.rb
+++ Casks/coronasdk-nightly.rb
@@ -1,13 +1,17 @@
 cask :v1 => 'coronasdk-nightly' do
+
   version '2015.2646'
   sha256 :no_check
+
   url "https://developer.coronalabs.com/sites/default/files/corona/#{version}/CoronaSDK-#{version}.dmg"
+
   name 'Corona SDK'
   homepage 'http://coronalabs.com/products/corona-sdk'
   license :gratis
 
-  suite "CoronaSDK-#{version.split('.')[1]}" #, :target => 'CoronaSDK-nightly'
-
+  #suite "CoronaSDK-#{version.split('.')[1]}" #, :target => 'CoronaSDK-nightly'
+  suite "CoronaSDK"
+  
   # Renamed for consistency.
   # app "CoronaSDK-#{version.split('.')[1]}"
 
