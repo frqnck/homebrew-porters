5966c913f930516c342ab0a039e7869bae80f11a
Franck Cassedanne <franck@ouarz.net>
2015-06-01 19:27:02 +0100

    Fix.

--- Casks/coronasdk-nightly.rb
+++ Casks/coronasdk-nightly.rb
@@ -2,17 +2,11 @@ cask :v1 => 'coronasdk-nightly' do
 
   version '2015.2646'
   sha256 :no_check
-
   url "https://developer.coronalabs.com/sites/default/files/corona/#{version}/CoronaSDK-#{version}.dmg"
 
   name 'Corona SDK'
   homepage 'http://coronalabs.com/products/corona-sdk'
   license :gratis
 
-  #suite "CoronaSDK-#{version.split('.')[1]}" #, :target => 'CoronaSDK-nightly'
-  suite "CoronaSDK"
-  
-  # Renamed for consistency.
-  # app "CoronaSDK-#{version.split('.')[1]}"
-
+  suite "CoronaSDK-#{version.split('.')[1]}", :target => '/Applications/CoronaSDK'
 end
