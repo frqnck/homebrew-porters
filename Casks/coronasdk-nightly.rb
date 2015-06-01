72452e2cb2ab778d633546f83d344681bb315739
Franck Cassedanne <franck@ouarz.net>
2015-06-01 17:50:22 +0100

    Initial import.

---
+++ Casks/coronasdk-nightly.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'coronasdk-nightly' do
+  version '2015.2632'
+  sha256 :no_check
+  url "https://developer.coronalabs.com/sites/default/files/corona/#{version}/CoronaSDK-#{version}.dmg"
+  name 'Corona SDK'
+  homepage 'http://coronalabs.com/products/corona-sdk'
+  license :gratis
+
+  suite "CoronaSDK-#{version.split('.')[1]}" #, :target => 'CoronaSDK-nightly'
+
+  # Renamed for consistency.
+  # app "CoronaSDK-#{version.split('.')[1]}"
+
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Porters
+
+https://assets-cdn.github.com/images/icons/emoji/unicode/1f303.png
+
+
+This is a Homebrew/Cask Tap containing a collection of *nightly-built* formulae:
+
+## Usage
+
+First, tap this repo
+
+	brew tap frqnck/homebrew-porters
+
+Next, install a Cask
+	
+	brew cask coronasdk-nightly
