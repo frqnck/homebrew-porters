17c6eeda565a7b7ac2ade844715cd062c028ba78
Franck Cassedanne <franck@ouarz.net>
2015-11-26 11:46:15 +0000

    Upped  to 2015.2776

--- Casks/coronasdk-nightly.rb
+++ Casks/coronasdk-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'coronasdk-nightly' do
 
-  version '2015.2775'
+  version '2015.2776'
   sha256 :no_check
   url "https://developer.coronalabs.com/sites/default/files/corona/#{version}/CoronaSDK-#{version}.dmg"
 
